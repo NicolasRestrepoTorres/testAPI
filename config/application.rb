b2162a81b9a03cf522860092f130b5956f1c40f6
lizzyt10h <nrestrepot@unal.edu.co>
2017-10-19 03:46:22 +0000

    Rspec configurations

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,5 @@ module TestAPI
     config.api_only = true
   end
   
-  
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
 
 end
