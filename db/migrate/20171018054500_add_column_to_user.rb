41e4122533e054570bed9560929e2be4ab20f678
lizzyt10h <nrestrepot@unal.edu.co>
2017-10-19 04:30:01 +0000

    Detail in migration for Heroku

--- db/migrate/20171018054500_add_column_to_user.rb
+++ db/migrate/20171018054500_add_column_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :name, :string
+    add_column :users, :name, :string unless User.column_names.include?('name')
     add_column :users, :token, :string
     add_column :users, :email, :string
     add_column :users, :password_digest, :string
